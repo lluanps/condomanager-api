5ae30a37bfbe1e13951864576419fe69cca32071
Luan Pinheiro da Silva <lluanps@gmail.com>
2025-07-28 22:07:42 -0300

    config inicial

---
+++ condomanager-api/src/main/java/com/lluanps/condomanager_api/CondomanagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.lluanps.condomanager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondomanagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondomanagerApiApplication.class, args);
+	}
+
+}
---
+++ condomanager-api/src/test/java/com/lluanps/condomanager_api/CondomanagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lluanps.condomanager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondomanagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
